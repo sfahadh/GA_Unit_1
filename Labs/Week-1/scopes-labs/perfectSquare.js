f27119a9fd826285c1ab26765fb8004f60c5ff97
Fahad Hussain <sfahadh14@gmail.com>
2019-04-26 23:23:19 -0400

    added scopes lab

---
+++ Labs/Week-1/scopes-labs/perfectSquare.js
@@ -0,0 +1,28 @@
+// const perfectSquare = (num) => {
+// 	return (Math.sqrt(num) % 1 == 0);
+// }
+
+// console.log(perfectSquare(25));
+
+
+// const checkToLimit = (num) => {
+// 	for(let i = 1; i <= num; i++) {
+// 		Math.sqrt(i) % 1 == 0 ? console.log(i) : console.log("filler");
+// 	}
+// }
+
+// console.log(checkToLimit(16));
+
+for(var i = 0; i < 5; i++) {
+	console.log("I want to eat more", + i);
+}
+
+console.log("is there more food?", + i);
+
+console.log(" ")
+
+for(let i = 0; i < 5; i++) {
+	console.log("I want to eat more", + i);
+}
+
+console.log("is there more food?", + i);
