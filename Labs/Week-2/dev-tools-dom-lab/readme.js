3d75d91adb7860048c1b09414dd722d7f0d0a872
Fahad Hussain <sfahadh14@gmail.com>
2019-04-27 15:49:28 -0400

    added dev tools dom lab

---
+++ Labs/Week-2/dev-tools-dom-lab/readme.js
@@ -0,0 +1,33 @@
+// vanilla-solution.js
+/* eslint no-unused-vars: 0 */
+/* eslint-env browser, node */
+
+// TODO: [1] Select the <body>
+document.querySelector("body");
+
+// TODO: [2] Select the <header>
+document.querySelector("head");
+
+// TODO: [3] Select all of the elements with class "quote"
+document.getElementsByClassName("")
+// TODO: [4] Select all of the subject elements
+
+// TODO: [5] Select the navigation links
+
+// TODO: [6] Select all the quotes in the "life" section.
+
+// TODO: [7] Using relative selection, select the <p> element containing the second Mark Twain quote
+
+// TODO: [8] Do the same using a query selector
+
+// TODO: [9] Select the 'Quotes About Motivation' heading
+
+// TODO: [10] -  Add a class 'simplicity' to the first subject.
+
+
+// BONUS
+// TODO: [11] Create an element with the text of your favorite quote.
+
+// TODO: [12] Add the element that you created in the previous step to the layout by appending it to a corresponding subject section.
+
+// TODO: [13] Add the author's last name, as a class name, to one of the quotes.
